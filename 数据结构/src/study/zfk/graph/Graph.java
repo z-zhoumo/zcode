3e1d7664a4df42885c51767a1bed132d93572540
user <1>
2021-10-26 17:11:26 +0800

    1

--- 数据结构/src/study/zfk/graph/Graph.java
+++ 数据结构/src/study/zfk/graph/Graph.java
@@ -1,13 +1,18 @@
 package study.zfk.graph;
 
+import javax.swing.*;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Graph {
 
     private ArrayList<String> vertexList; //存储顶点的集合
     private int [][] edges;//存储图对应的邻结矩阵
     private int numOfEdges;//表示边的数目
+    //定义给数组boolean[]，记录某个结点是否被访问
+    private boolean[] isVisited ;
 
     public static void main(String[] args) {
 
@@ -28,6 +33,12 @@ public class Graph {
 
         graph.showGraph();
 
+        //测试dfs遍历
+//        System.out.println("深度遍历");
+//        graph.dfs();
+
+        System.out.println("\n****广度优先******");
+        graph.bfs();
     }
 
 
@@ -37,11 +48,108 @@ public class Graph {
         edges = new int[n][n];
         vertexList = new ArrayList<String>(n);
         numOfEdges = 0;
+        isVisited = new boolean[5];
+
+    }
+
+    //得到第一个邻接结点的下标 w
+
+    /**
+     *
+     * @param index
+     * @return 如果存在就返沪对应的下标，否则返回 -1
+     */
+    public int getFirstNeighbor(int index){
+        for (int i = 0; i < vertexList.size(); i++) {
+            if(edges[index][i] > 0){
+                return i ;
+            }
+        }
+        return -1;
+    }
+    //根据前一个邻接结点的下标来获取下一个临界结点
+    public int getNextNeighbor(int v1,int v2){
+        for (int i = v2 +1; i < vertexList.size(); i++) {
+            if(edges[v1][i] > 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //深度优先遍历算法
+    //i 第一次为 0
+    public void dfs(boolean [] isVisited,int i){
+        //首先我们访问该结点，输出
+        System.out.print(getValueByIndex(i)+"--> ");
+        //将结点设置为已经访问
+        isVisited[i] = true;
+        //查找结点 i 的第一个邻接结点 w
+        int w = getFirstNeighbor(i);
+        while (w != -1){
+            if(! isVisited[w]){
+                dfs(isVisited,w);
+            }
+            //如果 w 结点已经被访问过
+            w = getNextNeighbor(i,w);
+        }
+    }
+
+
+    //对 dfs 进行一个重载，遍历哦我们所有的结点，并进行 dfs
+    public void dfs(){
+        //遍历所有的结点，进行 dfs （回溯）
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if(! isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+
+    //对一个结点进行广度优先遍历的方法
+    private void bfs(boolean[] isVisited,int i){
+        int u ; // 表示队列的头结点对应下标
+        int w ; //邻接结点 w
+        // 队列，记录结点访问的顺序
+        LinkedList queue = new LinkedList();
+        //访问结点，输出结点的信息
+        System.out.print(getValueByIndex(i)+ "==> ");
+        //标记为已访问
+        queue.addLast(i);
+
+        while( ! queue.isEmpty()){
+            //取出队列头结点的下标
+            u = (Integer) queue.removeFirst();
+            //得到第一个邻结点的下标 w
+            w = getFirstNeighbor(u);
+            while (w != -1){//找到
+                //是否访问过
+                if(! isVisited[w]){
+                    System.out.print(getValueByIndex(w) + "==> ");
+                    //标记已经访问过
+                    isVisited[w] = true;
+                    //入队
+                    queue.addLast(w);
+                }
+                //以 u 为前驱，找 w 后面的下一个邻结点
+                w = getNextNeighbor(u,w);//体现广度优先
+            }
+        }
+    }
+
+    //遍历所有的结点，都进行广度优先搜索
+    public void bfs(){
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if(! isVisited[i]){
+                bfs(isVisited,i);
+            }
+        }
     }
 
     //图中常用的方法
     //返回结点的个数
-    public int getNumOfEVertex(){
+    public int getNumOfVertex(){
         return vertexList.size();
     }
     //得到边的数目
